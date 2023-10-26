0002cf4e2cb1e715959720db9912be8c5e8e7d25
Eddie Song <tcsong@berkeley.edu>
2023-10-25 23:30:54 -0700

    removed openai key from main, added more data

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ import pandas as pd
 
 from PyPDF2 import PdfReader
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from setup import setup_openAI
 
-os.environ["OPENAI_API_KEY"] = "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"
+setup_openAI()
 
 # documents = SimpleDirectoryReader("YOUR_DATA_DIRECTORY").load_data()
 # index = VectorStoreIndex.from_documents(documents)
